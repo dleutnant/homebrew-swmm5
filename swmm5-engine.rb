45fbd9019d5b9db19fa85e0ce7d90e57d73a2065
dleutnant <leutnant@fh-muenster.de>
2018-11-24 00:45:41 +0100

    test

--- swmm5-engine.rb
+++ swmm5-engine.rb
@@ -8,15 +8,8 @@ class Swmm5Engine < Formula
   sha256 "1626a3421f2cdfcee6c6e07ba720a4cca1a537b5d2fc99fd0d15147fce289c59"
   depends_on "libomp"
   
-  fails_with :clang do
-    build 001
-    cause "-c flag error"
-  end
-
   def install
     ENV.deparallelize
-    ENV.delete('CFLAGS')
-    ENV.delete('CXXFLAGS')
     system "make"
   end
 
