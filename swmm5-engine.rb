8334798641b0dc90e60b393dad20f431e6c0b964
dleutnant <leutnant@fh-muenster.de>
2018-11-23 23:40:21 +0100

    openmp and make

--- swmm5-engine.rb
+++ swmm5-engine.rb
@@ -6,9 +6,10 @@ class Swmm5Engine < Formula
   homepage ""
   url "https://github.com/dleutnant/swmm5-engine/archive/v5.1.13.tar.gz"
   sha256 "1626a3421f2cdfcee6c6e07ba720a4cca1a537b5d2fc99fd0d15147fce289c59"
+  depends_on "openmp"
 
   def install
-    system "make", "install" # if this fails, try separate make/make install steps
+    system "make"
   end
 
 end
