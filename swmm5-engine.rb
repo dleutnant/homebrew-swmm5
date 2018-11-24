431f9821d72c717357e10c61db8733836bbb48f2
dleutnant <leutnant@fh-muenster.de>
2018-11-24 10:14:57 +0100

    test

--- swmm5-engine.rb
+++ swmm5-engine.rb
@@ -2,8 +2,8 @@ class Swmm5Engine < Formula
   desc
   homepage ""
   url "https://github.com/dleutnant/swmm5-engine/archive/v5.1.13.tar.gz"
-  sha256 "1626a3421f2cdfcee6c6e07ba720a4cca1a537b5d2fc99fd0d15147fce289c59"
-  revision 2
+  sha256 "185dac3dc86b00a4ea8b45214ff3141c561fa53be97aab277258448c4b25a825"
+  revision 3
   depends_on "libomp"
 
   def install
