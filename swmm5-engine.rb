e3b01169a2773dc412e413c13708dd2d93958919
dleutnant <leutnant@fh-muenster.de>
2018-11-24 09:15:53 +0100

    test

--- swmm5-engine.rb
+++ swmm5-engine.rb
@@ -1,16 +1,12 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Swmm5Engine < Formula
   desc ""
   homepage ""
   url "https://github.com/dleutnant/swmm5-engine/archive/v5.1.13.tar.gz"
   sha256 "1626a3421f2cdfcee6c6e07ba720a4cca1a537b5d2fc99fd0d15147fce289c59"
   depends_on "libomp"
-  
+
   def install
     ENV.deparallelize
     system "make"
   end
-
 end
